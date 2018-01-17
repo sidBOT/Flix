c009ef75549a329d2015a9efb876ca8cd885c1cc
Siddhant Patel <si12hant@gmail.com>
2018-01-17 18:58:48 -0500

    Added images to tab view

---
+++ Flix/PosterCell.swift
@@ -0,0 +1,15 @@
+//
+//  PosterCell.swift
+//  Flix
+//
+//  Created by siddhant on 1/17/18.
+//  Copyright © 2018 siddhant. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterImage: UIImageView!
+}
